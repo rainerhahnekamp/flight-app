1a9d2658f01d2f1feda47440e784d2ead66ba2b3
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-12-02 12:17:57 +0100

    feat: improve flights

--- src/app/model/flight.ts
+++ src/app/model/flight.ts
@@ -5,25 +5,3 @@ export interface Flight {
   date: string;
   delayed: boolean;
 }
-
-const flight: Flight = {
-  id: 1,
-  from: 'SFO',
-  to: 'JFK',
-  date: '2022-01-01',
-  delayed: false,
-};
-
-Object.freeze(flight);
-
-type Color = 'red' | 'green' | 'blue';
-
-type ColorMap = Record<Color, string>;
-
-function addFlight(flight: Readonly<Flight>) {}
-
-const colorMap: ColorMap = {
-  red: '#FF0000',
-  green: '#00FF00',
-  blue: '#0000FF',
-};
